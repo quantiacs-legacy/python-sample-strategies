8741b337a00fb56eb5859054727e7b643f975aa7
Yalei <yaleis@alumni.cmu.edu>
2017-01-31 10:52:35 -0800

    minor syntax changes and longEquity in meanReversion.py

--- trendFollowing.py
+++ trendFollowing.py
@@ -2,24 +2,25 @@
 # import necessary Packages below:
 import numpy
 
+
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
     ''' This system uses trend following techniques to allocate capital into the desired equities'''
 
-    nMarkets=CLOSE.shape[1]
+    nMarkets = CLOSE.shape[1]
 
-    periodLonger=200
-    periodShorter=40
+    periodLonger = 200
+    periodShorter = 40
 
     # Calculate Simple Moving Average (SMA)
-    smaLongerPeriod=numpy.nansum(CLOSE[-periodLonger:,:],axis=0)/periodLonger
-    smaShorterPeriod=numpy.nansum(CLOSE[-periodShorter:,:],axis=0)/periodShorter
+    smaLongerPeriod = numpy.nansum(CLOSE[-periodLonger:, :], axis=0)/periodLonger
+    smaShorterPeriod = numpy.nansum(CLOSE[-periodShorter:, :], axis=0)/periodShorter
 
-    longEquity= smaShorterPeriod > smaLongerPeriod
-    shortEquity= ~longEquity
+    longEquity = smaShorterPeriod > smaLongerPeriod
+    shortEquity = ~longEquity
 
-    pos=numpy.zeros(nMarkets)
-    pos[longEquity]=1
-    pos[shortEquity]=-1
+    pos = numpy.zeros(nMarkets)
+    pos[longEquity] = 1
+    pos[shortEquity] = -1
 
     weights = pos/numpy.nansum(abs(pos))
 
@@ -29,7 +30,7 @@ def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, setting
 def mySettings():
     ''' Define your trading system settings here '''
 
-    settings= {}
+    settings = {}
 
     # S&P 100 stocks
     # settings['markets']=['CASH','AAPL','ABBV','ABT','ACN','AEP','AIG','ALL',
@@ -44,17 +45,17 @@ def mySettings():
 
     # Futures Contracts
 
-    settings['markets']  = ['CASH','F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CC', 'F_CD',
-    'F_CL', 'F_CT', 'F_DX', 'F_EC', 'F_ED', 'F_ES', 'F_FC','F_FV', 'F_GC',
-    'F_HG', 'F_HO', 'F_JY', 'F_KC', 'F_LB', 'F_LC', 'F_LN', 'F_MD', 'F_MP',
-    'F_NG', 'F_NQ', 'F_NR', 'F_O', 'F_OJ', 'F_PA', 'F_PL', 'F_RB', 'F_RU',
-    'F_S','F_SB', 'F_SF', 'F_SI', 'F_SM', 'F_TU', 'F_TY', 'F_US','F_W', 'F_XX',
-    'F_YM']
-    # settings['beginInSample'] = '20120506'
-    # settings['endInSample'] = '20150506'
-    settings['lookback']= 504
-    settings['budget']= 10**6
-    settings['slippage']= 0.05
+    settings['markets'] = ['CASH', 'F_AD', 'F_BO', 'F_BP', 'F_C', 'F_CC', 'F_CD',
+                           'F_CL', 'F_CT', 'F_DX', 'F_EC', 'F_ED', 'F_ES', 'F_FC', 'F_FV', 'F_GC',
+                           'F_HG', 'F_HO', 'F_JY', 'F_KC', 'F_LB', 'F_LC', 'F_LN', 'F_MD', 'F_MP',
+                           'F_NG', 'F_NQ', 'F_NR', 'F_O', 'F_OJ', 'F_PA', 'F_PL', 'F_RB', 'F_RU',
+                           'F_S', 'F_SB', 'F_SF', 'F_SI', 'F_SM', 'F_TU', 'F_TY', 'F_US', 'F_W',
+                           'F_XX', 'F_YM']
+    settings['beginInSample'] = '20120506'
+    settings['endInSample'] = '20150506'
+    settings['lookback'] = 504
+    settings['budget'] = 10**6
+    settings['slippage'] = 0.05
 
     return settings
 
